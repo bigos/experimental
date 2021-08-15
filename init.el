2dd3afe2ed174847f6d876346bb7d4aff9101c2a
Jacek Podkanski <ruby.object@googlemail.com>
2021-08-15 13:51:02 +0100

    ignore tangled files and modify init.el

--- init.el
+++ init.el
@@ -1,2 +1,6 @@
-(org-babel-load-file (expand-file-name "literal-config.org"  user-emacs-directory) 1)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(org-babel-load-file
+ (expand-file-name "literal-config.org"  user-emacs-directory) 1)
 
