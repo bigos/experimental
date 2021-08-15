14b1a756790a0d763e4524cb0bc60cd303ef1543
Jacek Podkanski <ruby.object@googlemail.com>
2021-08-16 00:54:16 +0100

    init.el automatically compiles literal config

--- init.el
+++ init.el
@@ -4,6 +4,11 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(org-babel-load-file
- (expand-file-name "literal-config.org"  user-emacs-directory) 1)
+(defvar org-config (expand-file-name "literal-config.org" init-dir))
+(defvar elc-config (expand-file-name "literal-config.elc" init-dir))
 
+(if (file-newer-than-file-p org-config elc-config)
+    (progn
+      (message "going to tangle literal config.")
+      (org-babel-load-file (expand-file-name org-config  user-emacs-directory) 1))
+  (load-file elc-config)) 
