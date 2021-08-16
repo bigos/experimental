3d1708adfee864693dc57facf77c3f4dd65893db
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2021-08-16 14:53:32 +0100

    at last we can automatically install packages

--- init.el
+++ init.el
@@ -1,13 +1,10 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-
 (defvar init-dir (file-name-directory load-file-name)
   "The root dir of the Emacs config.")
 
 (defvar org-config (expand-file-name "literal-config.org" init-dir))
 (defvar elc-config (expand-file-name "literal-config.elc" init-dir))
 
-(if (file-newer-than-file-p org-config elc-config)
+(if (> 2 1) ;(file-newer-than-file-p org-config elc-config)
     (progn
       (message "going to tangle literal config.")
       (org-babel-load-file (expand-file-name org-config  user-emacs-directory) 1))
