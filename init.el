88e1627eef6065c0455210bbee343d6d96b40593
Jacek Podkanski <ruby.object@googlemail.com>
2023-01-08 19:17:38 +0000

    made it work with emacs 29

--- init.el
+++ init.el
@@ -6,8 +6,20 @@
 (defvar elc-config (expand-file-name "literal-config.elc" init-dir)
   "Path to the tangled and compiled configuration.")
 
-(message "going to tangle literal config.")
-(org-babel-load-file org-config )
+
+
+(org-babel-load-file
+ org-config
+ (if (file-newer-than-file-p
+      org-config
+      (expand-file-name "literal-config.el" init-dir))
+     (progn
+       (message "YES going to tangle literal config.")
+       t)
+   (progn
+     (message "NOT going to tangle literal config.")
+     nil)))
 
 
 
+(put 'upcase-region 'disabled nil)
