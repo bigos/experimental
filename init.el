3b7609e12dba482f83567fb21198463b1d2ca447
Jacek Podkanski <ruby.object@googlemail.com>
2023-01-08 18:38:58 +0000

    config fix

--- init.el
+++ init.el
@@ -6,10 +6,8 @@
 (defvar elc-config (expand-file-name "literal-config.elc" init-dir)
   "Path to the tangled and compiled configuration.")
 
-(if (file-newer-than-file-p org-config elc-config)
-    (progn
-      (message "going to tangle literal config.")
-      (org-babel-load-file (expand-file-name org-config  user-emacs-directory) 1))
-  (load-file elc-config)) 
+(message "going to tangle literal config.")
+(org-babel-load-file org-config )
+
 
 
