faf83f6759a400e53f2710be257012652b74aa3a
Jacek Podkanski <ruby.object@googlemail.com>
2025-02-25 18:37:40 +0000

    copied new changes

--- init.el
+++ init.el
@@ -1,12 +1,33 @@
-(defvar init-dir (file-name-directory load-file-name)
+(defvar init-dir (file-name-directory (or load-file-name
+                                          user-init-file))
   "Directory of the Emacs config init.el.")
 
+;; -----------------------------------------------------------------------------
+;; Define package source.
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+(message "package archives used %s" package-archives)
+
+;; set package-user-dir to be relative to init.el path
+(setq package-user-dir (expand-file-name "elpa" init-dir))
+
+;; -----------------------------------------------------------------------------
+
+;; This is the list of packages to install. Remember, installing packages from
+;; melpa does not mean loading the into  Emacs.
+
+
+;; -----------------------------------------------------------------------------
 (defvar org-config (expand-file-name "literal-config.org" init-dir)
   "Path to the literal configuration..")
 
-;; -----------------------------------------------------------------------------
 (org-babel-load-file org-config)
 
-
-
-
+;;; end of init
+;; -----------------------------------------------------------------------------
